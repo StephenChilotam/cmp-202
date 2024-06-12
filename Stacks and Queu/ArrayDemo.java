f1672a98d70bd930cefc069cad35443773351024
StephenChilotam <130247576+StephenChilotam@users.noreply.github.com>
2024-06-12 14:20:49 +0100

    Add files via upload

---
+++ Stacks and Queu/ArrayDemo.java
@@ -0,0 +1,20 @@
+package SAQ;
+
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+
+        int[] numbersArray = new int[10];
+        numbersArray[0] = 17;
+        numbersArray[1] = 36;
+        numbersArray[2] = 38;
+        numbersArray[3] = 77;
+        numbersArray[4] = 47;
+
+
+        for (int i = 0; i<numbersArray.length; i++) {
+            System.out.println(numbersArray[i]);
+        }
+
+    }
+}
